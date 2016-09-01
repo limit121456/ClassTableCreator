a7190336f87f6e357232e8567cb5fb241a0f7c87
CodeOfGame <havefun121456@gmail.com>
2016-09-01 17:30:31 +0800

    版本1
    版本1

---
+++ 根据类生成表结构/根据类生成表结构/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace 根据类生成表结构
+{
+    public partial class Form1 : Form
+    {
+        //获取连接字符串
+        public string conn=ConfigurationManager.ConnectionStrings["Conn"].ConnectionString.ToString();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection conn = new SqlConnection(this.conn))
+            {
+                try
+                {
+                    conn.Open();
+                    label1.Text = "连接成功";
+                }
+                catch
+                {
+                    label1.Text = "连接失败";
+                }
+            }
+        }
+
+    }
+}
